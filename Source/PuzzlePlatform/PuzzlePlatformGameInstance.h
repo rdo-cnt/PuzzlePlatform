e4395fcc47d8db7c57289cce18609537594be581
Watulion <rdo_cnt@hotmail.com>
2017-12-05 21:16:02 -0600

    When to use a gameinstance

---
+++ Source/PuzzlePlatform/PuzzlePlatformGameInstance.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PuzzlePlatformGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORM_API UPuzzlePlatformGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	UPuzzlePlatformGameInstance(const FObjectInitializer & ObjectInitializer);
+	//Init or construct?
+	void Init();
+		
+	
+};
